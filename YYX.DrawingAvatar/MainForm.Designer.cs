16a73c6adeecd857f0511f59eafaaae3841f77b9
yinyongxian <yinyongxian@qq.com>
2019-10-01 23:00:56 +0800

    Add project files.

---
+++ YYX.DrawingAvatar/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace YYX.DrawingAvatar
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelRadius = new System.Windows.Forms.Label();
+            this.comboBoxRadius = new System.Windows.Forms.ComboBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(460, 408);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
+            // 
+            // labelRadius
+            // 
+            this.labelRadius.AutoSize = true;
+            this.labelRadius.Location = new System.Drawing.Point(12, 16);
+            this.labelRadius.Name = "labelRadius";
+            this.labelRadius.Size = new System.Drawing.Size(29, 12);
+            this.labelRadius.TabIndex = 1;
+            this.labelRadius.Text = "半径";
+            // 
+            // comboBoxRadius
+            // 
+            this.comboBoxRadius.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxRadius.FormattingEnabled = true;
+            this.comboBoxRadius.Items.AddRange(new object[] {
+            "16",
+            "32",
+            "64",
+            "128",
+            "256",
+            "512",
+            "1024"});
+            this.comboBoxRadius.Location = new System.Drawing.Point(47, 13);
+            this.comboBoxRadius.Name = "comboBoxRadius";
+            this.comboBoxRadius.Size = new System.Drawing.Size(344, 20);
+            this.comboBoxRadius.TabIndex = 2;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(397, 11);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 3;
+            this.buttonOK.Text = "确定";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.comboBoxRadius);
+            this.Controls.Add(this.labelRadius);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "画头像";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelRadius;
+        private System.Windows.Forms.ComboBox comboBoxRadius;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
+
---
+++ YYX.DrawingAvatar/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+
+namespace YYX.DrawingAvatar
+{
+    public partial class MainForm : Form
+    {
+
+        private Bitmap bitmap;
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBoxRadius.SelectedIndex = 3;
+            buttonOK_Click(null, null);
+            SaveImage(null, null);
+        }
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            int radius;
+            var tryParse = int.TryParse(comboBoxRadius.Text, out radius);
+            if (!tryParse)
+            {
+                MessageBox.Show(@"请设置正确的半径");
+                return;
+            }
+
+            var sideLength = 2 * radius;
+            bitmap = new Bitmap(sideLength, sideLength);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            var pen = new Pen(Color.Black);
+            var size = new Size(sideLength, sideLength);
+            var location = new Point(0, 0);
+            var rectangle = new Rectangle(location, size);
+            graphics.DrawEllipse(pen, rectangle);
+
+            pictureBox.Image = bitmap;
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            ContextMenuStrip = null;
+
+            if (e.Button != MouseButtons.Right)
+            {
+                return;
+            }
+
+            var contextMenuStrip = new ContextMenuStrip();
+            var toolStripItem = contextMenuStrip.Items.Add("图片另存为");
+            toolStripItem.Click += SaveImage;
+
+            ContextMenuStrip = contextMenuStrip;
+            contextMenuStrip.Show(MousePosition);
+        }
+
+        private void SaveImage(object sender, EventArgs e)
+        {
+            if (bitmap == null)
+            {
+                return;
+            }
+
+            var folderBrowserDialog = new FolderBrowserDialog();
+            var dialogResult = folderBrowserDialog.ShowDialog();
+            if (dialogResult != DialogResult.OK)
+            {
+                return;
+            }
+            var selectedPath = folderBrowserDialog.SelectedPath;
+
+            var filename = $"Avator-{DateTime.Now.ToString("yyyyMMddhhmmss")}.bmp";
+            var path = Path.Combine(selectedPath, filename);
+            bitmap.Save(path);
+        }
+    }
+}
